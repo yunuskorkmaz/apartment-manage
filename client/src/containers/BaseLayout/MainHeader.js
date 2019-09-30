3e2c8c38cf6d00177557dff267612def2bd99085
Yunus Korkmaz <yunuskorkmaz95@gmail.com>
2019-09-30 16:21:03 +0300

    header rightcontent

--- client/src/containers/BaseLayout/MainHeader.js
+++ client/src/containers/BaseLayout/MainHeader.js
@@ -1,27 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Layout,Button} from 'antd'
-
+import { Layout, Button, Icon, Menu, Dropdown, Tabs } from 'antd'
+import 'antd/es/style/themes/default.less';
 
 import './mainHeader.css'
 
 const { Header } = Layout;
+const { TabPane } = Tabs;
 function MainHeader(props){
-    const { children, onCollapse,...rest} = props;
+    const { children, collapsed, onCollapse,...rest} = props;
 
     const renderCollapsedButton = ()=>{
         const {renderCollapseButton} = props;
 
-        return(
-            <Button onClick={onCollapse} type={"dashed"}>
-                Collapse
-            </Button>
+        return renderCollapseButton
+                ? (
+                <span className={"collapse-trigger"} onClick={onCollapse}>
+                        <Icon type={collapsed ? 'menu-unfold' : 'menu-fold'} />
+                    </span>
+                )
+                : <></>
+            
+        
+    }
+
+    const renderHeaderContent = () =>{
+
+        const menu = (
+            <Tabs defaultActiveKey="1" >
+                <TabPane tab="Tab 1" key="1">
+                    Content of Tab Pane 1
+                </TabPane>
+                <TabPane tab="Tab 2" key="2">
+                    Content of Tab Pane 2
+                </TabPane>
+                <TabPane tab="Tab 3" key="3">
+                    Content of Tab Pane 3
+                </TabPane>
+            </Tabs>
+        );
+
+        return (
+            <Dropdown overlay={menu}>
+                <a className="ant-dropdown-link" href="#">
+                    Hover me <Icon type="down" />
+                </a>
+            </Dropdown>
+
         )
     }
 
     return(
-        <Header  >
+        <Header  theme={'light'}>
             {renderCollapsedButton()}
+            <div style={{float:'right',marginRight:'100px'}}>
+                {renderHeaderContent()}
+            </div>
             {children}
         </Header>
     )
--- client/src/containers/BaseLayout/index.js
+++ client/src/containers/BaseLayout/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Layout, Button } from 'antd'
+import { Layout, Button,Tabs } from 'antd'
 import 'antd/dist/antd.css'
 import useMedia from "react-media-hook2";
 import MainSider from "./MainSider";
 import MainHeader from './MainHeader';
 
 const { Content, Footer } = Layout;
+const { TabPane } = Tabs
 
 function Main(props) {
     const [collapse, setCollapse] = React.useState(true);
@@ -22,8 +23,22 @@ function Main(props) {
             <Layout style={{ height: "100vh" }}>
                 <MainSider collapse={collapse} onCollapse={val => setCollapse(val)} isMobile={isMobile} />
                 <Layout>
-                    <MainHeader onCollapse={() => setCollapse(!collapse)}/>
-                    <Content>asdasd</Content>
+                    <MainHeader collapsed={collapse} onCollapse={() => setCollapse(!collapse)}/>
+                    <Content>
+                    
+                        <Tabs defaultActiveKey="1" >
+                            <TabPane tab="Tab 1" key="1">
+                                Content of Tab Pane 1
+                </TabPane>
+                            <TabPane tab="Tab 2" key="2">
+                                Content of Tab Pane 2
+                </TabPane>
+                            <TabPane tab="Tab 3" key="3">
+                                Content of Tab Pane 3
+                </TabPane>
+                        </Tabs>
+                    
+                    </Content>
                     <Footer>Footer</Footer>
                 </Layout>
             </Layout>
