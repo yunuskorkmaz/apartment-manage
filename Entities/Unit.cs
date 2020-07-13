e0eb454fbb22f293f5d17448a135770cc920bcae
Yunus Korkmaz <yunuskorkmaz95@gmail.com>
2020-07-13 21:45:30 +0300

    clear

--- Entities/Unit.cs
+++
@@ -1,9 +0,0 @@
-namespace apartment_manage.Entities
-{
-    public class Unit
-    {
-        public int Id { get; set; }
-        public int No { get; set; }
-        public bool Status { get; set; }
-    }
-}
--- Models/Result.cs
+++
@@ -1,9 +0,0 @@
-namespace apartment_manage.Models
-{
-    public class Result<T>
-    {
-        public bool success { get; set; }
-        public string message { get; set; }
-        public T data { get; set; }
-    }
-}
